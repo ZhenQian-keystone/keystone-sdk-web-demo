bc22d887c00e29706898f43bc4dc87c71b62c02f
LiYanLance <liyan1924@gmail.com>
2023-05-19 11:31:55 +0800

    Update key derivation call data

--- src/wallet/KeyDerivationCall.js
+++ src/wallet/KeyDerivationCall.js
@@ -2,7 +2,7 @@ import KeystoneSDK from "@keystonehq/keystone-sdk";
 import {AnimatedQRCode} from "@keystonehq/animated-qr";
 
 export const KeyDerivationCall = () => {
-    const paths = ["m/44'/60'/0'", "m/44'/501'/0'"];
+    const paths = ["m/44'/60'/0'", "m/84'/0'/0'"];
     const ur = KeystoneSDK.generateKeyDerivationCall({ paths });
     return <AnimatedQRCode type={ur.type} cbor={ur.cbor.toString("hex")}/>
 }
