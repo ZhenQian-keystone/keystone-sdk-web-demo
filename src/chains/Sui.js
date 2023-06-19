25bd2bd59a8bdf4230e0d5e62c4602fca0be88b9
Renfeng Shi <renfeng@keyst.one>
2023-06-19 14:56:19 +0800

    fix sui

--- src/chains/Sui.js
+++ src/chains/Sui.js
@@ -1,15 +1,15 @@
-import KeystoneSDK, {KeystoneSuiSDK, UR, URType} from "@keystonehq/keystone-sdk"
+import KeystoneSDK, { UR, URType } from "@keystonehq/keystone-sdk"
 import {AnimatedQRCode, AnimatedQRScanner} from "@keystonehq/animated-qr"
+import { useCallback, useState } from 'react';
 
 let suiSignRequest = {
     requestId: "7AFD5E09-9267-43FB-A02E-08C4A09417EC",
-    signData: "000002002086ac6179ca6ad9a7b1ccb47202d06ae09a131e66309944922af9c73d3c203b66000810270000000000000202000101010001010200000100000e4d9313fb5b3f166bb6f2aea587edbe21fb1c094472ccd002f34b9d0633c71901d833a8eabc697a0b2e23740aca7be9b0b9e1560a39d2f390cf2534e94429f91ced0c00000000000020190ca0d64215ac63f50dbffa47563404182304e0c10ea30b5e4d671b7173a34c0e4d9313fb5b3f166bb6f2aea587edbe21fb1c094472ccd002f34b9d0633c719e803000000000000640000000000000000",
-    signType: KeystoneSuiSDK.SignType.Single,
+    intentMessage: "00000000000200201ff915a5e9e32fdbe0135535b6c69a00a9809aaf7f7c0275d3239ca79db20d6400081027000000000000020200010101000101020000010000ebe623e33b7307f1350f8934beb3fb16baef0fc1b3f1b92868eec3944093886901a2e3e42930675d9571a467eb5d4b22553c93ccb84e9097972e02c490b4e7a22ab73200000000000020176c4727433105da34209f04ac3f22e192a2573d7948cb2fabde7d13a7f4f149ebe623e33b7307f1350f8934beb3fb16baef0fc1b3f1b92868eec39440938869e803000000000000640000000000000000",
     accounts:[
         {
             path: "m/44'/784'/0'/0'/0'",
-            xfp: '78230804',
-            address: '0e4d9313fb5b3f166bb6f2aea587edbe21fb1c094472ccd002f34b9d0633c719'
+            xfp: 'f23f9fd2',
+            address: '0xebe623e33b7307f1350f8934beb3fb16baef0fc1b3f1b92868eec39440938869'
         }
     ],
     origin: "Sui Wallet"
@@ -18,20 +18,29 @@ let suiSignRequest = {
 export const Sui = () => {
     const keystoneSDK = new KeystoneSDK();
     const ur = keystoneSDK.sui.generateSignRequest(suiSignRequest);
+    const [isScan, setIsScan] = useState(false);
 
-    return <AnimatedQRCode type={ur.type} cbor={ur.cbor.toString("hex")}/>
+    const scan = useCallback(() => {
+        setIsScan(!isScan);
+    }, [isScan]);
+
+    return <>
+        <AnimatedQRCode type={ur.type} cbor={ur.cbor.toString("hex")}/>
+        <button onClick={scan}>Scan Keystone</button>
+        {isScan && <SuiScanner />}
+    </>
 }
 
-// export const SuiScanner = () => {
-//     const keystoneSDK = new KeystoneSDK();
-//
-//     const onSucceed = ({type, cbor}) => {
-//         const signature = keystoneSDK.sui.parseSignature(new UR(Buffer.from(cbor, "hex"), type))
-//         console.log("signature: ", signature);
-//     }
-//     const onError = (errorMessage) => {
-//         console.log("error: ",errorMessage);
-//     }
-//
-//     return <AnimatedQRScanner handleScan={onSucceed} handleError={onError} urTypes={[URType.SuiSignature]} />
-// }
+export const SuiScanner = () => {
+    const keystoneSDK = new KeystoneSDK();
+
+    const onSucceed = ({type, cbor}) => {
+        const signature = keystoneSDK.sui.parseSignature(new UR(Buffer.from(cbor, "hex"), type))
+        console.log("signature: ", signature);
+    }
+    const onError = (errorMessage) => {
+        console.log("error: ",errorMessage);
+    }
+
+    return <AnimatedQRScanner handleScan={onSucceed} handleError={onError} urTypes={[URType.SuiSignature]} options={{ width: 100 }} />
+}
